8890b5d92f711d7f8fac9f1206c81380d018c9db
Thomas Massmann <thomas.massmann@it-spir.it>
2018-04-26 11:04:14 +0200

    Add postbox 5.

---
+++ Casks/postbox_5.rb
@@ -0,0 +1,23 @@
+cask 'postbox' do
+  version '5.0.24,1_c5810ea796337b300b4f30dac967fb7623b5a16c'
+  sha256 '124704184e020d41cdbc7e587aea9a04cf8ccae6216ee330d895d8a827fc2665'
+
+  # amazonaws.com/download.getpostbox.com was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/download.getpostbox.com/installers/#{version.before_comma}/#{version.after_comma}/postbox-#{version.before_comma}-mac64.dmg"
+  name 'Postbox'
+  homepage 'https://www.postbox-inc.com/'
+
+  depends_on macos: '>= :mavericks'
+
+  app 'Postbox.app'
+
+  zap trash: [
+               '~/Library/Application Support/Postbox',
+               '~/Library/Caches/com.crashlytics.data/com.postbox-inc.postbox',
+               '~/Library/Caches/com.postbox-inc.postbox',
+               '~/Library/Caches/Postbox',
+               '~/Library/PDF Services/Mail PDF with Postbox',
+               '~/Library/Preferences/com.postbox-inc.postbox.plist',
+               '~/Library/Saved Application State/com.postbox-inc.postbox.savedState',
+             ]
+end
